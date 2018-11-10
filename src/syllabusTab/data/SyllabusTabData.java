a19ab99204c0a94a1a056d045f177f385cc30309
MichaelMorales219 <michael.morales@stonybrook.edu>
2018-11-10 15:32:36 -0500

    Syllabus Tab Text Areas added with hidden capability

---
+++ src/syllabusTab/data/SyllabusTabData.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syllabusTab.data;
+
+import coursesite.CourseSiteApp;
+import djf.components.AppDataComponent;
+
+/**
+ *
+ * @author Michael
+ */
+public class SyllabusTabData implements AppDataComponent {
+    CourseSiteApp app;
+    
+    
+    public SyllabusTabData(CourseSiteApp initApp) {
+        CourseSiteApp app;
+    }
+    
+    @Override
+    public void reset() {
+        
+    }
+}
